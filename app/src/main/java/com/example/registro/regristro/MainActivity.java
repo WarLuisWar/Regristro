284bfd76ebcc41f814c30464bfc3b92d8ea97019
Luis Carlos Contreras <manilla13@hotmail.com>
2016-02-26 08:29:30 -0500

    Agregué un comentario en el Main, para demostrar mi amistad con GitHub

--- app/src/main/java/com/example/registro/regristro/MainActivity.java
+++ app/src/main/java/com/example/registro/regristro/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //GitHub es mi mejor amigo de la vida
+    //<3
+    //So jarcor
 
 }
